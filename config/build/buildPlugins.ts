8d64d4f041efea571d047d52b65acef0faa065df
Sergey Margelov <smargelov@gmail.com>
2023-05-18 21:38:54 +0400

    Add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,8 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import { BuildOptions } from './types/config'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import type { BuildOptions } from './types/config'
 
 export default function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
 	const loaders: webpack.WebpackPluginInstance[] = [
@@ -20,7 +21,10 @@ export default function buildPlugins({ paths, isDev }: BuildOptions): webpack.We
 	const devLoaders: webpack.WebpackPluginInstance[] = [
 		// плагин для работы с горячей перезагрузкой
 		new ReactRefreshWebpackPlugin({ overlay: false }),
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false
+		})
 	]
 	// плагины для продакшена
 	const prodLoaders: webpack.WebpackPluginInstance[] = [
